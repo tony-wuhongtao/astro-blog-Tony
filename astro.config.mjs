44bf8a5f89e78d40b25053085f7acdd6f5b6aa30
Tony Wu <tony.wuhongtao@gmail.com>
2025-08-02 19:25:08 +0800

    blog with page and pagination is done

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,12 +5,15 @@ import tailwindcss from '@tailwindcss/vite';
 
 import svelte from '@astrojs/svelte';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
-
+  output: 'server',
   site: 'http://tonywu.top/',
   integrations: [svelte()],
+  adapter: netlify(),
 });
