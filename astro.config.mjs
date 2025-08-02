e39475c2eced38c24866b119e2604e7c0e3473d2
Tony Wu <tony.wuhongtao@gmail.com>
2025-08-02 17:00:40 +0800

    builded contact form with kwesforms.com

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,14 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import svelte from '@astrojs/svelte';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
+
   site: 'http://tonywu.top/',
+  integrations: [svelte()],
 });
---
+++ src/components/ContactForm.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+    import kwesforms from 'kwesforms';  
+    import {onMount} from 'svelte';
+
+    onMount(() => {
+
+        kwesforms.init();
+
+    });
+</script>
+
+<form method="POST" action="https://kwesforms.com/api/f/K5ar2WuGVs26ym4O3DYb" class="kf-form grid gap-y-2"
+    no-error-message
+    redirect="/thankyou">
+
+    <label for="comment" class="text-zinc-900 text-2xl mb-2 mt-10">
+        What's on your mind?
+    </label>
+    <textarea class="border border-teal-900 rounded-lg text-2xl px-6 py-6"
+    name="comment" id="comment" rows="3"  
+    rules="required|max:255" >
+
+    </textarea>
+    <button type="submit" class="text-2xl text-zinc-100 bg-teal-900 px-6 py-5 rounded-xl my-1 cursor-pointer hover:bg-teal-600">
+        Send
+    </button>
+
+</form>  
