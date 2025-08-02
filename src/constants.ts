c49a70f6b47a521afae253e5125cbd9ca1b4e5b2
Tony Wu <tony.wuhongtao@gmail.com>
2025-08-02 16:01:45 +0800

    ssg blog basit finish

---
+++ src/constants.ts
@@ -0,0 +1,5 @@
+export const SITE_TITLE = "Tony Wu's blog";
+export const SITE_DESCRIPTION =
+  '独立开发者，专注Web技术研发。个人总结、技术文档、技术翻译，包括但不限于前端、后端、产品经理、AI应用、Web应用架构等。';
+export const HOMEPAGE_ARTICLE_LIMIT = 6;
+export const ARTICLES_PER_PAGE = 6;
---
+++ src/pages/rss.xml.ts
@@ -0,0 +1,25 @@
+import rss from "@astrojs/rss"
+import {getCollection} from "astro:content"
+import sanitizeHtml from "sanitize-html"
+import MarkdownIt from "markdown-it"
+import type {APIContext} from "astro"
+
+const parser = new MarkdownIt()
+
+export async function GET(context:APIContext){
+    const posts = await getCollection("posts");
+    return rss({
+        title: "Tony's Blog",
+        description: "A blog about technology",
+        site: context.site?.toString() ?? "",
+        items: posts.map(post => ({
+            title: post.data.title,
+            link: `/blog/${post.slug}`,
+            pubDate: new Date(post.data.date),
+            content: sanitizeHtml(parser.render(post.body)),
+            image: post.data.image,
+        })),
+    })
+}
+
+
